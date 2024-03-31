4331828f1889f7399aa734b48d20e82379e17aa7
Ataix <atai_ks@yahoo.com>
2024-03-31 15:37:32 +0600

    fixed order serializer

--- src/order/serializers.py
+++ src/order/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Order
 from product.serializers import ProductSerializer
 
+from product.models import Product
+
 
 class OrderSerializer(serializers.ModelSerializer):
     items = ProductSerializer(many=True)
@@ -14,3 +16,12 @@ class OrderSerializer(serializers.ModelSerializer):
             'user',
             'items'
         )
+
+    def create(self, validated_data):
+        items = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        order.save()
+        for item in items:
+            item = Product.objects.create(**item)
+            order.items.add(item)
+        return order
